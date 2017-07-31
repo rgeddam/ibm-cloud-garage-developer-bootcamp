289ac509af43a7869cda629e18f42af00086992b
Rajasekhar Geddam <rajasekhar.geddam@gmail.com>
2017-07-31 11:01:28 -0500

    stack initial code

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -4,19 +4,24 @@ const makeStack = () => {
 
   const isEmpty = () => stackSize === 0;
   const push = () => stackSize++;
+  const pop = () => stackSize--;
   const size = () => stackSize;
 
   return {
     isEmpty,
     push,
+    pop,
     size
   };
 };
 
-let stack = makeStack();
-
 describe.only('the stack spec', () => {
 
+  let stack;
+
+  beforeEach( () => {
+    stack = makeStack();
+  });
 
   it('starts empty', () => {
     stack.isEmpty().should.be.true();
@@ -33,12 +38,24 @@ describe.only('the stack spec', () => {
   });
 
   it('leaves stack size 1 when pushed', () => {
-    //stack.push();
+    stack.push();
     stack.size().should.equal(1);
   });
 
-  it('leaves stack empty when pushed and popped');
-  it('leaves stack size 0 when pushed and popped');
+  it('leaves stack empty when pushed and popped', () => {
+    stack.isEmpty().should.be.true();
+    stack.push();
+    stack.pop();
+    stack.isEmpty().should.be.true();
+  });
+
+  it('leaves stack size 0 when pushed and popped', () => {
+    stack.push();
+    stack.pop();
+    console.log('leaves stack size 0 when pushed and popped ' + stack.size());
+    stack.size().should.equal(0);
+  });
+
   it('overflows');
   it('under-flows');
   it('pops the same one pushed');
