337051e99ed6286f61bbe8154aab290b2ab16821
Rajasekhar Geddam <rajasekhar.geddam@gmail.com>
2017-07-31 10:25:22 -0500

    stack initial code

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,13 +1,42 @@
-describe.only('the stack spec', () => {
-  let stack = {
-    isEmpty: () => true
+
+const makeStack = () => {
+  let stackSize = 0;
+
+  const isEmpty = () => stackSize === 0;
+  const push = () => stackSize++;
+  const size = () => stackSize;
+
+  return {
+    isEmpty,
+    push,
+    size
   };
+};
+
+let stack = makeStack();
+
+describe.only('the stack spec', () => {
+
+
   it('starts empty', () => {
     stack.isEmpty().should.be.true();
   });
-  it('starts with stack size 0');
-  it('is not be empty when pushed');
-  it('leaves stack size 1 when pushed');
+
+  it('starts with stack size 0', () => {
+    stack.size().should.equal(0);
+  });
+
+  it('is not be empty when pushed', () => {
+    stack.push();
+    console.log('Stack size is ' + stack.size());
+    stack.isEmpty().should.be.false();
+  });
+
+  it('leaves stack size 1 when pushed', () => {
+    //stack.push();
+    stack.size().should.equal(1);
+  });
+
   it('leaves stack empty when pushed and popped');
   it('leaves stack size 0 when pushed and popped');
   it('overflows');
